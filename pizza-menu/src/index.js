291b2decec2234d5e169d32360cdc8be10e872e0
nexola <vitormatheusfv@gmail.com>
2024-03-13 11:34:30 -0300

    conditional rendering with ternary operator

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -73,12 +73,14 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {pizzas.length > 0 && (
+      {pizzas.length > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
     </main>
   );
@@ -107,11 +109,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
