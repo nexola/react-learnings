03cd2029d0259f61fa94ff1ac450802b88cdd686
nexola <vitormatheusfv@gmail.com>
2024-03-14 18:28:42 -0300

    React fragment

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -74,11 +74,17 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {pizzas.length > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later :)</p>
       )}
